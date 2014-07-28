1235c0dfae32d1fd6f44953e2fa7f524783577c9
Brajesh Kumar <kbrajesh176@gmail.com>
2014-07-28 22:22:59 +0530

    spoj 1

---
+++ spoj/TOANDFRO.cpp
@@ -0,0 +1,122 @@
+// =====================================================================================
+//
+//       Filename:  TOANDFRO.cpp
+//
+//    Description:  http://www.spoj.com/problems/TOANDFRO/
+//
+//        Version:  1.0
+//        Created:  Monday 28 July 2014 09:37:40  IST
+//       Revision:  none
+//       Compiler:  g++
+//
+//         Author:  Brajesh Kumar (), kbrajesh176@gmail.com
+//   Organization:  
+//
+// =====================================================================================
+
+
+//Data Structure includes
+#include<vector>
+#include<stack>
+#include<set>
+#include<bitset>
+#include<map>
+#include<queue>
+#include<deque>
+#include<string>
+
+
+//Other Includes
+#include<iostream>
+#include<fstream>
+#include<algorithm>
+#include<cstring>
+#include<cassert>
+#include<cstdlib>
+#include<cstdio>
+#include<cmath>
+
+using namespace std;
+
+#define FOR(i,a,b) for(int i=a;i<b;i++)
+#define REP(i,n) FOR(i,0,n)
+#define pb push_back
+#define mp make_pair
+#define s(n) scanf("%d",&n)
+#define sl(n) scanf("%lld",&n)
+#define sf(n) scanf("%lf",&n)
+#define ss(n) scanf("%s",n)
+#define fill(a,v) memset(a, v, sizeof a)
+#define sz(a) int((a).size())
+#define INF (int)1e9
+#define EPS 1e-9
+#define bitcount __builtin_popcount
+#define all(c) (c).begin(), (c).end()
+#define maX(a,b) (a>b?a:b)
+#define miN(a,b) (a<b?a:b)
+#define DREP(a) sort(all(a)); a.erase(unique(all(a)),a.end())
+#define INDEX(arr,ind) lower_bound(all(arr),ind)-arr.begin()
+#define tr(c,i) for(typeof((c).begin()) i = (c).begin(); i != (c).end(); i++)
+#define present(c,x) ((c).find(x) != (c).end())
+#define cpresent(c,x) (find(all(c),x) != (c).end())
+
+typedef vector<int> VI;
+typedef vector<vector<int> > VVI;
+typedef long long LL;
+typedef vector<long long > VLL;
+typedef pair<int, int > PII;
+typedef vector< PII > VPII;
+/*Main Code*/
+#define EXIT_SUCCESS 0
+// ===  FUNCTION  ======================================================================
+//         Name:  main
+//  Description:  main function
+// =====================================================================================
+	int
+main ( int argc, char *argv[] ){
+    
+    int n;
+    string str;
+    while(s(n) && n){
+        cin>>str;
+        int len = str.length();
+        int row = len/n;
+        char grid[row][n];
+        //cout<<"row  "<<row<<"  column  "<<n<<endl;
+        REP(i, row){
+            REP(j, n){
+                int index;
+                if(i & 1){
+                    // odd index
+                    index = (n - j - 1) ;
+                } else {
+                    index = j;
+                }
+                grid[i][index] = str[i * n + j];
+            }
+        }
+
+
+        string ret = "";
+        REP(i, n){
+            REP(j, row){
+                ret += grid[j][i];
+            }
+        }
+
+        /*
+        // print grid
+        REP(i, row){
+            REP(j, n){
+                cout<<grid[i][j];
+            }
+
+            cout<<"\n";
+        }
+        */
+        cout<<ret<<"\n";
+    }
+
+	return EXIT_SUCCESS;
+}		// ----------  end of function main  ---------- 
+
